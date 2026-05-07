0a4ab02f6e2fca6a79c32f835a1084e394d444ab
Aaisha Khalfan Saif AlSaidi <alsaidiaaisha@gmail.com>
2026-05-07 11:53:26 +0400

    Add files via upload

---
+++ project01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace InheritanceExample
+{
+    // Parent Class
+    class Animal
+    {
+        // Normal Function
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+
+        // Virtual Function
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal makes a sound");
+        }
+
+        // Virtual Function
+        public virtual void Move()
+        {
+            Console.WriteLine("Animal is moving");
+        }
+    }
+
+    // Child Class
+    class Dog : Animal
+    {
+        // Override Function
+        public override void Sound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+
+        // Override Function
+        public override void Move()
+        {
+            Console.WriteLine("Dog runs");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object from Child Class
+            Dog dog1 = new Dog();
+
+            dog1.Eat();
+            dog1.Sound();
+            dog1.Move();
+        }
+    }
+}
